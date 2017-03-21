755ff639ba3c198eefa5f38e378e61b5be16cf9e
Scott Baker <smbaker@gmail.com>
2017-03-20 19:16:02 -0700

    CORD-879 eliminate unnecessary import of Subscriber model
    Change-Id: I0496bdefe20fb97d52400dc4cd8d51730baf0947

--- xos/models.py
+++ xos/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from core.models import Service, PlCoreBase, Slice, Instance, Tenant, TenantWithContainer, Node, Image, User, Flavor, Subscriber
+from core.models import Service, PlCoreBase, Slice, Instance, Tenant, TenantWithContainer, Node, Image, User, Flavor
 from core.models.plcorebase import StrippedCharField
 import os
 from django.db import models, transaction
