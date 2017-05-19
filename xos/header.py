baf92789e2a3e1a6bfed07168a68ce5c66c0d7b7
Sapan Bhatia <gwsapan@gmail.com>
2017-05-19 23:10:52 +0200

    CORD-1345: Rename PlCoreBase to XOSBase
    Change-Id: Ide26de9abc0cc6d0a9c5c92362b9717ad51af105

--- xos/header.py
+++ xos/header.py
@@ -1,1 +1,13 @@
-attic/header.py
+from django.db import models
+from core.models import Service, XOSBase, Slice, Instance, Tenant, TenantWithContainer, Node, Image, User, Flavor
+from core.models.xosbase import StrippedCharField
+import os
+from django.db import models, transaction
+from django.forms.models import model_to_dict
+from django.db.models import *
+from operator import itemgetter, attrgetter, methodcaller
+import traceback
+from xos.exceptions import *
+from core.models import SlicePrivilege, SitePrivilege
+from sets import Set
+
